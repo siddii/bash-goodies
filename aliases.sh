f3a1670d22b09db01c01e7ff1916d3dfb6f5b8b4
Siddique Hameed <siddii+git@gmail.com>
2014-10-30 12:12:53 -0500

    adding 'serve' & 'o' aliases

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+alias serve="python -m SimpleHTTPServer"
+#o alias will only work on Mac OS
+alias o="open"
--- bash-utils.sh
+++ bash-utils.sh
@@ -5,3 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR"/cd-dash-dash.bash
 source "$DIR"/git-profile.sh
 source "$DIR"/git-aliases.sh
+source "$DIR"/aliases.sh
